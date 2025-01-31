edb93b77f27b51946c20402e31de8480f4ff0f13
user <196348511+faipena@users.noreply.github.com>
2025-01-31 10:52:04 +0100

    multiple methods handlers

--- bot/src/router.ts
+++ bot/src/router.ts
@@ -7,8 +7,13 @@ export interface HandlerFunction {
   ): Response | Promise<Response> | undefined;
 }
 
+export enum RequestMethod {
+  GET = "GET",
+  POST = "POST",
+}
+
 export interface RequestHandler {
-  method: "GET" | "POST";
+  methods: RequestMethod[] | "*";
   path: RegExp | string;
   handle: HandlerFunction;
 }
@@ -43,22 +48,24 @@ export default class Router {
   }
 
   get(path: string | RegExp, handle: HandlerFunction) {
-    this.add({ method: "GET", path, handle });
+    this.add({ methods: [RequestMethod.GET], path, handle });
   }
 
   post(path: string | RegExp, handle: HandlerFunction) {
-    this.add({ method: "POST", path, handle });
+    this.add({ methods: [RequestMethod.POST], path, handle });
   }
 
   listen(): Deno.HttpServer {
     return Deno.serve(this.#options, (
       request: Request,
-      info: RequestInfo
+      info: RequestInfo,
     ) => {
       let result: Response | Promise<Response> | undefined;
       this.#handlers.forEach((handler) => {
-        const url = new URL(request.url);
-        if (request.method === handler.method && url.pathname.match(handler.path)) {
+        const methodMatches = handler.methods === "*" ||
+          handler.methods.some((method) => method === request.method);
+        const pathMatches = new URL(request.url).pathname.match(handler.path);
+        if (methodMatches && pathMatches) {
           result = handler.handle(request, info);
         }
       });
