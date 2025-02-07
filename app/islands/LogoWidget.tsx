f9be793fa440b1d48898724b94042d06fbacdb2e
user <196348511+faipena@users.noreply.github.com>
2025-02-07 23:14:24 +0100

    update

--- app/islands/LogoWidget.tsx
+++ app/islands/LogoWidget.tsx
@@ -1,11 +1,11 @@
 export default function LogoWidget() {
   return (
     <>
-      <div class="min-h-screen flex flex-col items-center justify-center p-8">
+      <div class="min-h-screen flex flex-col items-center  p-8">
         {/* Logo */}
         <div class="mb-8">
           <img
-            src="/logo.svg"
+            src="/logo-dark.svg"
             width="128"
             height="128"
             alt="the Fresh logo: a sliced lemon dripping with juice"
@@ -17,6 +17,7 @@ export default function LogoWidget() {
             <span class="text-red-500 font-bold">LIVE</span>
             <div class="live-dot"></div>
           </div>
+          
           <h1 class="py-3 font-godzilla text-6xl font-bold text-purple-500">
             GodzillaZ
           </h1>
