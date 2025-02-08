d42cbe900984403e039e911eeef8273d8ac77745
TyKonKet <matteoautieri@live.it>
2025-02-08 15:54:26 +0100

    add bot token validation to live status update handler

--- app/routes/api/v1/live/index.ts
+++ app/routes/api/v1/live/index.ts
@@ -2,15 +2,21 @@ import { FreshContext } from "$fresh/server.ts";
 import { Handlers } from "$fresh/server.ts";
 import AppState from "../../../../appState.ts";
 
+const BOT_TOKEN = Deno.env.get("BOT_TOKEN");
+
 export const handler: Handlers = {
-  GET(_req: Request, ctx: FreshContext) {
+  GET(_req: Request, _ctx: FreshContext) {
     return new Response(JSON.stringify({ isLive: AppState.isLive }));
   },
 
-  async PUT(_req: Request, ctx: FreshContext) {
-    if (_req.headers.get("bot-token") === "iMtheB0t") {
-      const { isLive } = await _req.json();
-      AppState.isLive = isLive;
+  async PUT(req: Request, _ctx: FreshContext) {
+    if (!BOT_TOKEN) {
+      return new Response("Server configuration error", { status: 500 });
+    }
+    if (req.headers.get("bot-token") === BOT_TOKEN) {
+      const body = await req.json();
+      console.info("Live status update requested ", body);
+      AppState.isLive = body.isLive;
       return new Response();
     }
     return new Response("Unauthorized", { status: 401 });
